f85bdc7064aa3630f750635804a8c35d808fa704
igv <igv.versn@gmail.com>
2017-11-21 22:52:26 +0300

    Implemented NewsEntry::Authored::Create and NewsEntry::Authored::Update

--- app/interactors/news_entry/authored/create.rb
+++ app/interactors/news_entry/authored/create.rb
@@ -1,4 +1,15 @@
-class AuthoredNewsEntry::Create
+class NewsEntry::Authored::Create
+  include Interactor
 
+  def call
+    news_entry = NewsEntry::Authored.create(context.params)
 
+    if news_entry.persisted?
+      context.news_entry = news_entry
+      context.flash_message = { notice: "News created successfully" }
+    else
+      context.flash_message = { alert: "Creation failed" }
+      context.fail!
+    end
+  end
 end
--- app/interactors/news_entry/authored/update.rb
+++ app/interactors/news_entry/authored/update.rb
@@ -1,3 +1,14 @@
-class AuthoredNewsEntry::Update
+class NewsEntry::Authored::Update
+  include Interactor
 
+  def call
+    news_entry = NewsEntry::Authored.find(context.news_entry_id)
+    news_entry.update(context.params)
+
+    if news_entry.persisted?
+      context.news_entry = news_entry
+    else
+      context.fail!
+    end
+  end
 end
