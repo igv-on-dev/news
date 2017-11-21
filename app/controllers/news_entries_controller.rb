e0bf18df197e66c0d99d7e1a5b715f7d34532e5e
igv <igv.versn@gmail.com>
2017-11-21 15:40:56 +0300

    Controllers

---
+++ app/controllers/news_entries_controller.rb
@@ -0,0 +1,9 @@
+class NewsEntriesController < ApplicationController
+  def index
+  end
+
+  private
+
+  def find_main_news_entry
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "news_entries#index"
+
+  namespace :admin do
+    root to: "news_entries#new"
+  end
 end
